fd31aa66fdc7685975f584f5efd189f9bc60ca42
Венер Мустаев <mustaev.03@mail.ru>
2021-12-06 20:45:02 +0500

    добавил одну строчку

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var pic = File.ReadAllBytes("hotel.jpg");
+            //var text = Convert.ToBase64String(pic);
+            //Console.WriteLine(text);
+            var client = new WebClient();
+            var response = client.DownloadString("http://localhost:50052/api/Hotels");
+            Console.WriteLine(response);
+            Console.WriteLine("eeee");
+            Console.ReadKey();
+        }
+    }
+}
